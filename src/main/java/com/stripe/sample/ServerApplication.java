9943bc84ada86ea018a019717b2ab22b0b9294fe
shubham bansal <shubh.6102001@gmail.com>
2024-01-20 13:00:41 +0530

    Payment Controller and endpoint api

---
+++ src/main/java/com/stripe/sample/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.stripe.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.stripe.sample")
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/stripe/sample/StripeController.java
@@ -0,0 +1,48 @@
+package com.stripe.sample;
+
+import com.google.gson.Gson;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping("/api")
+public class StripeController {
+    private final Gson gson = new Gson();
+    
+    static class CreatePaymentRequest {
+        private Long amount;
+        public Long getAmount() { return amount; }
+        public void setAmount(Long amount) { this.amount = amount; }
+    }
+
+    
+    static class CreatePaymentResponse {
+        private String clientSecret;
+        public CreatePaymentResponse(String clientSecret) { this.clientSecret = clientSecret; }
+    }
+
+    @PostMapping("/create-payment-intent")
+    public String createPaymentIntent(@RequestBody CreatePaymentRequest requestData) throws StripeException {
+    	
+    	Stripe.apiKey = "sk_test_51NTUMQSCuUXEi5M25tvRjpTIskOxZASOPWEnuU2zPfQayWRrIIHpU7QUuHfa5hjORUsYp8Rcrzo7cyy5FlbqvjpV00cxvCJRdy";
+    	
+        PaymentIntent paymentIntent = PaymentIntent.create(
+                PaymentIntentCreateParams.builder()
+                        .setAmount(requestData.amount*100)
+                        .setCurrency("inr")
+                        .build()
+        );
+        return gson.toJson(new CreatePaymentResponse(paymentIntent.getClientSecret()));
+    }
+
+}
